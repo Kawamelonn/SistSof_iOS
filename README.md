94b508477b684e2b42828fd7cda757fea3133432
CesarCeballosC <105232610+CesarCeballosC@users.noreply.github.com>
2023-08-31 19:18:51 -0500

    sel4c

--- README.md
+++
@@ -1,1 +0,0 @@
-# SistSof_iOS
---
+++ SEL4C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SEL4C
+//
+//  Created by César Andrés Ceballos Castillo on 30/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
